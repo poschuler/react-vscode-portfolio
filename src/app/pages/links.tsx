417859e3a1cae924b59b3c84a1aac7e7f5ae0e70
poschuler <poschuler@gmail.com>
2023-04-17 20:56:34 -0500

    first commit

---
+++ src/app/pages/links.tsx
@@ -0,0 +1,22 @@
+import { FaGithub, FaLinkedin, FaEnvelope } from 'react-icons/fa';
+
+export const links = [
+  {
+    index: 0,
+    title: 'Find me on Github',
+    href: 'https://github.com/poschuler',
+    icon: <FaGithub />,
+  },
+  {
+    index: 1,
+    title: 'Find me on LinkedIn',
+    href: 'https://www.linkedin.com/in/poschuler/',
+    icon: <FaLinkedin />,
+  },
+  {
+    index: 2,
+    title: 'Contact me via email',
+    href: 'poschuler@gmail.com',
+    icon: <FaEnvelope />,
+  },
+];
